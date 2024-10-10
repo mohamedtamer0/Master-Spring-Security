4bba2697b35bf6c4070e300259789a7ee009fe27
mohamedtamer0 <mt4452242@gmail.com>
2024-10-10 18:14:59 +0300

    update

--- BankBackend/src/main/java/com/tamer/BankBackend/BankBackendApplication.java
+++ BankBackend/src/main/java/com/tamer/BankBackend/BankBackendApplication.java
@@ -2,8 +2,10 @@ package com.tamer.BankBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan("com.tamer.BankBackend.controller.WelcomeController")
 public class BankBackendApplication {
 
 	public static void main(String[] args) {
