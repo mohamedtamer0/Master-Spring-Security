26eb69c4d932ab1da14df0b4e78ef8807b8d1685
mohamedtamer0 <mt4452242@gmail.com>
2024-10-13 01:28:32 +0300

    update

--- BankBackend/src/main/java/com/tamer/BankBackend/config/BankUserDetailsService.java
+++ BankBackend/src/main/java/com/tamer/BankBackend/config/BankUserDetailsService.java
@@ -12,11 +12,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BankUserDetailsService implements UserDetailsService {
-
 
     private final CustomerRepository customerRepository;
 
@@ -24,7 +24,8 @@ public class BankUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
                 UsernameNotFoundException("User details not found for the user: " + username));
-        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        List<GrantedAuthority> authorities = customer.getAuthorities().stream().map(authority -> new
+                SimpleGrantedAuthority(authority.getName())).collect(Collectors.toList());
         return new User(customer.getEmail(), customer.getPwd(), authorities);
     }
 }
